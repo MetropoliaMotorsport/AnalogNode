3bf1dc9aad3dad846e7c88bcc0ecb83bdff3221e
pyrophorol <hereticaljavascripter@gmail.com>
2020-03-13 17:32:12 +0200

    fixed problem, flash page 63 wasn't empty
    next: on to saving config to flash and reading it from flash

--- Src/config.c
+++ Src/config.c
@@ -46,7 +46,7 @@ void Config_0(void)
 	AnalogSensorBytes[3] = 0;
 	CanId_Diagnostics = 0x89;
 
-	SendAnalogPeriod = 10000; //in 100us resolution
+	SendAnalogPeriod = 1000; //in 100us resolution
 	CanSyncDelay = 0; //in 10us resolution
 
 	OverCurrentWarning = 1000;
